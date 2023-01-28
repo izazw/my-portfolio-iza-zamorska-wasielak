730fd515fb346d0566dda054589455b1ab857f38
izazw <iza@wasielak.eu>
2023-01-28 16:54:23 -0500

    javascript file, toggling classes, vissibility of the icons

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-document.querySelector()
+const xMarkButton = document.getElementById("x-mark");
+const burgerButton = document.getElementById("burger")
+const nav = document.getElementById("nav");
+console.log(nav)
+
+
+burgerButton.addEventListener("click", () => {
+   burgerButton.classList.toggle("mobile-icon-visibility")
+   xMarkButton.classList.toggle("mobile-icon-visibility")
+   nav.classList.toggle("mobile-nav")
+   }
+)
+
+xMarkButton.addEventListener("click", () => {
+   burgerButton.classList.toggle("mobile-icon-visibility")
+   xMarkButton.classList.toggle("mobile-icon-visibility")
+   nav.classList.toggle("mobile-nav")
+   }
+)
+
