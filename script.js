1ea2beb954c7ff36de32afe2cc518a7df67da2da
izazw <iza@wasielak.eu>
2023-01-29 19:21:06 -0500

    fix to sliding menu

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+//js sliding menu
+
 const xMarkButton = document.getElementById("x-mark");
 const burgerButton = document.getElementById("burger")
 const nav = document.getElementById("nav");
-console.log(nav)
 
 
 burgerButton.addEventListener("click", () => {
@@ -12,8 +13,21 @@ burgerButton.addEventListener("click", () => {
 )
 
 xMarkButton.addEventListener("click", () => {
-   burgerButton.classList.toggle("mobile-icon-visibility")
-   xMarkButton.classList.toggle("mobile-icon-visibility")
+   burgerButton.classList.toggle("mobile-icon-visibility");
+   xMarkButton.classList.toggle("mobile-icon-visibility");
    nav.classList.toggle("mobile-nav")
    }
 )
+
+const li = document.querySelectorAll("nav ul li");
+const liArray = [...li];
+
+liArray.forEach((item) => {
+   item.addEventListener("click", () => {
+      nav.classList.remove("mobile-nav");
+      xMarkButton.classList.toggle("mobile-icon-visibility");
+      burgerButton.classList.toggle("mobile-icon-visibility");
+   })
+})
+
+
